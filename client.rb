4cbcae1a8f18ee0c57472298425ba526c307c8eb
Prashanth3591 <ep3591@gmail.com>
2017-04-02 18:21:45 +0530

    Create client.rb

---
+++ client.rb
@@ -0,0 +1,12 @@
+
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "`hostname`"
+client_key               "#{current_dir}/`hostname`.pem"
+validation_client_name    "demo-validator"
+validation_key            "/etc/chef/demo-validator.pem"
+chef_server_url          "https://34.208.210.150/organizations/demo/"
+cookbook_path            ["#{current_dir}/../cookbooks"]
