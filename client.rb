dc57b1e75b1794865006cce0636273b6a8f15777
Prashanth3591 <ep3591@gmail.com>
2017-04-02 19:50:23 +0530

    Update client.rb

--- client.rb
+++ client.rb
@@ -1,11 +1,10 @@
-
 # See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
 
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "`hostname`"
-client_key               "#{current_dir}/`hostname`.pem"
+node_name                "HOST_NAME"
+client_key               "#{current_dir}/HOST_NAME.pem"
 validation_client_name    "demo-validator"
 validation_key            "/etc/chef/demo-validator.pem"
 chef_server_url          "https://34.208.210.150/organizations/demo/"
